93525467c83a213d824ce71b8182b8e4ca6fa5ff
Luka Jankovic <lukjan1999@gmail.com>
2023-10-31 09:51:16 +0100

    start dgpu alias

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ set fish_greeting
 
 starship init fish | source
 
+alias start_dgpu="__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia glxinfo &> /dev/null"
+
 function sudo
     if test "$argv" = !!
         eval command sudo $history[1]
